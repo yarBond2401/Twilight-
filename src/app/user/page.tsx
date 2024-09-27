a6f1ecbf0686454a05567ec82e33b1d8d7b684da
yaroslav <yarBond2401@gmail.com>
2024-09-27 17:44:31 +0300

    fixed the build issue

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,9 +1,16 @@
-"use client";
+'use client';
 
-import DomainSearch from "@/components/search/domainSearch";
+import {useAppDispatch} from "@/hooks";
+import UserPage from "@/currentPages/user/user";
+import {useEffect} from "react";
+import {fetchUserInfo} from "@/redux/user/thunks";
 
-export default function User() {
-    return (
-        <div>test1212</div>
-    );
+export default function Page() {
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUserInfo());
+    }, [dispatch]);
+
+    return <UserPage/>
 }
